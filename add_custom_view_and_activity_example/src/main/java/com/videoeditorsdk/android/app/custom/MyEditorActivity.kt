454038cfbf2a29643558ebc4203dd30a7e41e47c
Sven Nähler <sven.naehler@9elements.com>
2021-12-14 15:49:12 +0100

    Extend custom view and activity example.

--- add_custom_view_and_activity_example/src/main/java/com/videoeditorsdk/android/app/custom/MyEditorActivity.kt
+++ add_custom_view_and_activity_example/src/main/java/com/videoeditorsdk/android/app/custom/MyEditorActivity.kt
@@ -5,6 +5,12 @@ import android.os.PersistableBundle
 import android.widget.Toast
 import ly.img.android.pesdk.ui.activity.VideoEditorActivity
 import com.videoeditorsdk.android.app.R
+import ly.img.android.pesdk.assets.filter.basic.ColorFilterAssetBW
+import ly.img.android.pesdk.backend.filter.FilterAssetHatch
+import ly.img.android.pesdk.backend.model.EditorSDKResult
+import ly.img.android.pesdk.backend.model.state.FilterSettings
+import ly.img.android.pesdk.backend.model.state.LoadSettings
+import ly.img.android.pesdk.backend.model.state.manager.StateHandler
 import ly.img.android.pesdk.ui.activity.EditorActivity
 
 class MyEditorActivity : VideoEditorActivity() {
@@ -17,5 +23,24 @@ class MyEditorActivity : VideoEditorActivity() {
         // do something with it if you need to
         
         Toast.makeText(this, "Toast from custom Activity", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onCloseClicked() {
+        // Override Editor close behavior, to remove dialog
+        val stateHandler = stateHandler
+        val loadSettings = stateHandler[LoadSettings::class]
+        val result = EditorSDKResult.Builder(EditorSDKResult.Status.CANCELED).also {
+            it.setProduct(stateHandler.product)
+            it.setSourceUri(loadSettings.source)
+            it.setSettingsList(getStateHandler().createSettingsListDump())
+        }
+        setResult(result)
+        finish()
+    }
+
+    override fun onExportStart(stateHandler: StateHandler) {
+        // Change settings before starting export.
+        stateHandler[FilterSettings::class].filter = ColorFilterAssetBW()
+        super.onExportStart(stateHandler)
     }
 }
