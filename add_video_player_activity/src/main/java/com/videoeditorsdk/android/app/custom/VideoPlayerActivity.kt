086d8934448a10125a264151009b1d4a27d46434
Sven Nähler <sven.naehler@9elements.com>
2022-05-27 12:03:21 +0200

    Test version 10.2.0-alpha.1

---
+++ add_video_player_activity/src/main/java/com/videoeditorsdk/android/app/custom/VideoPlayerActivity.kt
@@ -0,0 +1,8 @@
+package com.videoeditorsdk.android.app.custom
+
+import ly.img.android.pesdk.ui.activity.EditorActivity
+import com.videoeditorsdk.android.app.R
+
+class VideoPlayerActivity : EditorActivity() {
+    override var layoutResource: Int = R.layout.imgly_activity_video_player
+}
